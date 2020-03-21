14d44c29eeeef6a433bc91c6eebab11ad80a08e2
Isla <podaneovd@mail.ru>
2020-03-21 22:50:38 +0300

    Добавьте файлы проекта.

---
+++ Factory_Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiLibrary;
+namespace Factory_Class
+{
+
+   
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathFactory factory = new MathFactory();
+
+
+
+            Console.WriteLine(factory.GetMathOperation(MathOperationType.Add).PerformMathAction(1, 4));
+            Console.WriteLine(factory.GetMathOperation(MathOperationType.Multiply).PerformMathAction(2, 4));
+
+        }
+    }
+}
---
+++ MiLibrary/MyFactoryClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MiLibrary
+{
+    public interface IMathOperation
+    {
+        int PerformMathAction(int oper1, int oper2);
+
+    }
+
+    public enum MathOperationType
+    {
+        Add = 0,
+        Multiply = 1
+
+    }
+
+    public class MathFactory
+    {
+
+        public IMathOperation GetMathOperation(MathOperationType MathOperType)
+        {
+            if (MathOperType == 0)
+            {
+                return new Adder();
+            }
+
+
+            return new Multiplier();
+        }
+
+
+        class Adder : IMathOperation
+        {
+
+
+            public int PerformMathAction(int oper1, int oper2)
+            {
+                return oper1 + oper2;
+            }
+        }
+
+
+        class Multiplier : IMathOperation
+        {
+            public int PerformMathAction(int oper1, int oper2)
+            {
+                return oper1 * oper2;
+            }
+        }
+
+    }
+
+}
