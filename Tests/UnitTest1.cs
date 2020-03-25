9a6114c5967e7a701262a39a84755e2b54b40f5f
Isla <podaneovd@mail.ru>
2020-03-25 11:29:44 +0300

    add Tests(2)

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,8 +8,11 @@ namespace Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MasterTest()
         {
+            MathFactory factory = new MathFactory();
+            Assert.AreEqual(10, factory.GetMathOperation(MathOperationType.Multiply).PerformMathAction(2, 5));
+            Assert.AreEqual(8, factory.GetMathOperation(MathOperationType.Add).PerformMathAction(2, 6));
         }
     }
 }
